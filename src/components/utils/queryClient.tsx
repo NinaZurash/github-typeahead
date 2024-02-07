d2e1056a0b3fe5441c89b8fdcb668f882f20fd3c
NinaZurash <nino.zurashvili.nz@gmail.com>
2024-02-07 22:20:07 +0400

    project init

--- src/components/utils/queryClient.tsx
+++ src/components/utils/queryClient.tsx
@@ -9,11 +9,12 @@ type Item = {
 export function UserSearchResults() {
   const { state } = useMainContext();
   const { isPending, error, data, refetch } = useQuery({
-    queryKey: ["repoData"],
+    queryKey: ["repoData", state], // Include state in the query key
     queryFn: () =>
       fetch(`https://api.github.com/search/users?q=${state}`).then((res) =>
         res.json()
       ),
+    staleTime: 60000,
   });
 
   useEffect(() => {
@@ -30,24 +31,26 @@ export function UserSearchResults() {
         An error has occurred:{error.message}
       </div>
     );
-  if (!data.items)
-    return (
-      <div className="flex justify-cente text-red-500">
-        Number of requests exceeded. Please try again later.
-      </div>
-    );
 
   return (
     <div className="flex flex-col mt-3">
-      {data.items.slice(0, 5).map((item: Item, index: number) => (
-        <div className="border-t p-2  border-zinc-200" key={index}>
-          {Object.keys(item).map((key: string, index: number) => (
-            <div key={index}>
-              <strong>{key}</strong> {item[key]}
-            </div>
-          ))}
+      {!data.items ? (
+        <div className="flex justify-cente text-red-500">
+          Number of requests exceeded. Please try again later.
         </div>
-      ))}
+      ) : data.items.length === 0 ? (
+        <div>No results found</div>
+      ) : (
+        data.items.slice(0, 5).map((item: Item, index: number) => (
+          <div className="border-t p-2  border-zinc-200" key={index}>
+            {Object.keys(item).map((key: string, index: number) => (
+              <div key={index}>
+                <strong>{key}</strong> {item[key]}
+              </div>
+            ))}
+          </div>
+        ))
+      )}
     </div>
   );
 }
